58063aca64191e7b3485bb4b18f11343765d5fcf
Sachin Kumar Kamat <36402247+easykamat@users.noreply.github.com>
2018-06-30 09:13:21 +0530

    Add files via upload

---
+++ 1. Write a program in C to display the first 10 natural numbers.c
@@ -0,0 +1,12 @@
+/*1. Write a program in C to display the first 10 natural numbers.
+Expected Output :
+1 2 3 4 5 6 7 8 9 10*/
+#include<stdio.h>
+void main()
+{
+  int i;
+  for(i=1;i<=10;i++)
+  {
+    printf("%d ",i);
+  }
+}
