322063482b725df9f78316ab1a3e18b9c5d9d2ac
Sachin Kumar Kamat <36402247+easykamat@users.noreply.github.com>
2018-06-30 13:10:02 +0530

    Update 2. Write a C program to find the sum of first 10 natural numbers.c

--- 2. Write a C program to find the sum of first 10 natural numbers.c
+++ 2. Write a C program to find the sum of first 10 natural numbers.c
@@ -1,4 +1,4 @@
-/*2. Write a C program to find the sum of first 10 natural numbers. Go to the editor
+/*2. Write a C program to find the sum of first 10 natural numbers.
 Expected Output :
 The first 10 natural number is :
 1 2 3 4 5 6 7 8 9 10
