c9994e4ed3fa5596d1c3aa98e25970d1bbb38893
Sachin Kumar Kamat <36402247+easykamat@users.noreply.github.com>
2018-06-30 15:16:48 +0530

    Add files via upload

---
+++ 5. Write a program in C to display the cube of the number upto given an integer.c
@@ -0,0 +1,21 @@
+/*5. Write a program in C to display the cube of the number upto given an integer.
+Test Data :
+Input number of terms : 5
+Expected Output :
+Number is : 1 and cube of the 1 is :1
+Number is : 2 and cube of the 2 is :8
+Number is : 3 and cube of the 3 is :27
+Number is : 4 and cube of the 4 is :64
+Number is : 5 and cube of the 5 is :125*/
+#include<stdio.h>
+void main()
+{
+  int num,i;
+  printf("Input number of terms: ");
+  scanf("%d",&num);
+  for(i=1;i<=num;i++)
+  {
+    printf("Number is:%d and cube of the %d is %d\n",i,i,(i*i*i));
+  }
+
+}
